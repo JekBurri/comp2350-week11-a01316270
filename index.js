0fd6f3241121faeaf8d0ec76d4055cb23f093c2e
Jack Boeri <53635108+JekBurri@users.noreply.github.com>
2023-03-23 15:27:22 -0700

    setup done

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ global.base_dir = __dirname;
 global.abs_path = function(path) {
 	return base_dir + path;
 }
+
 global.include = function(file) {
 	return require(abs_path('/' + file));
 }
-
-
 
 const express = require('express');
 const database = include('databaseConnection');
@@ -15,16 +14,15 @@ const router = include('routes/router');
 
 const port = process.env.PORT || 3000;
 
-database.getConnection((err, dbConnection) => {
+database.connect((err, dbConnection) => {
 	if (!err) {
-		console.log("Successfully connected to MySQL");
+		console.log("Successfully connected to MongoDB");
 	}
 	else {
-		console.log("Error Connecting to MySQL");
+		console.log("Error Connecting to MongoDB");
 		console.log(err);
 	}
 });
-
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -34,7 +32,7 @@ app.use(express.static(__dirname + "/public"));
 app.use('/',router);
 
 app.listen(port, () => {
-	console.log("Node application listening on port "+port);
+	console.log("Node application listening on port " + port);
 }); 
 
 
